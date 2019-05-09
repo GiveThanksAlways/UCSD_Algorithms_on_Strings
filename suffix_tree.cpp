fcccd63fa42435b1d129b0c57479db3417e0ed2b
Spencer Willett <spencer.willett15@gmail.com>
2019-05-09 01:48:06 +0000

    Clean up comments at the end add visualization link

--- suffix_tree.cpp
+++ suffix_tree.cpp
@@ -176,89 +176,10 @@ ACA$
 
 
 
-
-
-
-
-
+See visualization for help
+https://visualgo.net/en/suffixtree
 
 
 
 */
 
-
-/**
-vector <int> solve (const string& text, int n, const vector <string>& patterns)
-{
-	vector <int> result;
-
-	trie t = build_trie(patterns);
-  int j=0;
-  
-  for(auto & letter : text){// go through each letter in the text 
-    int node_ID = 0;
-    for(int i = j; i < text.size(); i++){// go from current letter to end of text 
-      char currentLetter = text[i];
-      // search all children nodes to see if one of the chilren match the current letter
-      bool hit = false;
-      for(auto & node : t[node_ID]){
-        if(node.first == currentLetter){// hit, we have a match 
-          hit = true;
-          node_ID = node.second;// go to the child
-          if(t[node_ID].find('$')->first == '$'){
-            // we reached the end of a pattern
-            char nextLetter = text[j+1];// get the next letter to look ahead to see if there is overlap 
-            if(t[node_ID].find(nextLetter)->second == 0){// nextLetter not in trie 
-              cout << j << " ";// no next letter, we hit the end anyways so lets keep going 
-              //cout << text[j] << text[j+1] << text[j+2] << endl;
-            }
-          }
-          break;// we found a match, end for loop running through the nodes 
-        }
-      }
-      if(!hit)// no match in the trie, so break second for loop 
-        break;
-    }
-    
-    j++;
-  }
-
-
-	return result;
-}*/
-
-/**
-trie build_trie(const vector<string> & patterns) {
-  trie t;
-  edges root;
-  t.push_back(root);
-  // go through all patterns
-  for(auto & daPattern : patterns){
-
-    int currentNode = 0;// root node
-    string CurrentPattern = daPattern;
-    //go through each letter of the pattern 
-    for(auto & currentSymbol : CurrentPattern){
-      //looks at the current node of mapped edges, finds if the letter key is in the map and returns 0 if it is not in the edges from the currentNode
-      // if the current Symbol is in the trie then don't add, just go to the next child in the tree
-      if( t[currentNode].find(currentSymbol)->second != 0){
-        currentNode = t[currentNode].find(currentSymbol)->second;
-      }else{
-        // currentSymbol not a child of the current node. We add the currentSymbol as a child of the node 
-        edges newNode;
-        t.push_back(newNode);
-        t[currentNode].insert(std::pair<char,int>(currentSymbol,t.size()-1));
-        currentNode = t.size()-1;
-      }
-      // note if the node is the end of a pattern in the trie 
-      if(CurrentPattern.at(CurrentPattern.length()-1) == currentSymbol){
-        // this is the end of pattern, mark the node with a $ symbol 
-        //cout << "end of pattern: " << daPattern << endl;
-        t[currentNode].insert(std::pair<char,int>('$',t.size()-1));
-
-      }
-          
-    }
-  }
-  return t;
-}*/
